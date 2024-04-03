ee2e818e6dbe66b756582e8fc34ad9ff6bb45646
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-04-03 15:46:31 +0200

    add files

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
   <script>
+  import Header from './components/Header.vue'
+  import Main from './components/Main.vue'
+  import Footer from './components/Footer.vue'
+
   export default {
+
+    components:{
+      Header,
+      Main,
+      Footer
+
+    },
+    data (){
+      return {
+  
+      }
+
+    }
   
   }
   </script>
 
 <template>
-  <div>
 
-  </div>
+  <Header />
+  <Main />
+  <Footer/>
+
 </template>
 
 
-<style >
+<style lang="scss" >
+@import './assets/scss/main.scss';
 
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+
+}
+</script>
+<template>
+
+  <header>
+    <div class="container">
+
+      <div>
+        logo
+      </div>
+
+      <div>
+        barra di ricerca
+      </div>
+
+    </div>
+  </header>
+
+</template>
+
+
+<style>
+
+</style>
