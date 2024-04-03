4a459bf6d6bcda4a162df4eb5032384a68b40cf8
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-04-03 16:07:16 +0200

    add changes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,11 +9,14 @@ export default {
     <div class="container d-flex justify-content-between py-3 ">
 
       <div class=" text-white ">
-        logo
+        <p class="my_logo">BOOLFIX</p>
       </div>
 
-      <div class=" text-white ">
-        barra di ricerca
+      <div class=" my-2">
+        <div class="input-group mb-4">
+          <input type="text" class="form-control" placeholder="cerca il tuo film" aria-label="Recipient's username" aria-describedby="button-addon2">
+          <button class="btn btn-danger" type="cerca" id="button-addon2">cerca</button>
+        </div>
       </div>
 
     </div>
@@ -27,6 +30,11 @@ header{
   height: 90px;
   background-color: black;
 
+}
+.my_logo{
+  font-size: 35px;
+  color: red;
+  font-weight: 600;
 }
 
 </style>
